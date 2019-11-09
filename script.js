abe4a9a11624a7a035e6f24878f229e4129097ff
Brian Ruff <briananthonyruff@gmail.com>
2019-11-08 20:04:20 -0500

    crud app in angular

---
+++ script.js
@@ -0,0 +1,16 @@
+angular.module("MyApp", []).controller("MainCtrl", function($scope) {
+  $scope.world = "Front end masters";
+  $scope.todoList = ["wash clothes", "clean up room"];
+  $scope.newItem = document.querySelector("#todo");
+  $scope.addTodo = function() {
+    $scope.newItem.value ? $scope.todoList.push($scope.newItem.value) : null;
+  };
+  $scope.removeTodo = function(index) {
+    $scope.todoList.splice(index, 1);
+  };
+  $scope.editTodo = function(index) {
+    const currentNode = document.querySelector(`#edit-${index}`);
+    const oldNode = document.querySelector(`#li-${index}`);
+    currentNode.value ? (oldNode.textContent = currentNode.value) : null;
+  };
+});
